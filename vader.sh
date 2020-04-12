dfb976d602689ba4b61759255008403693a047e5
Sebastian Butterweck <sebastian@butt3rweck.com>
2020-04-12 17:37:48 +0100

    reduced sox buffer

--- vader.sh
+++ vader.sh
@@ -1,2 +1,3 @@
+
 sleep 60s
-sudo nice --20 play "| BUF_0_=1 BUF_1_=1 rec --buffer 512 -d pitch -500 echos 0.8 0.88 10 0.6 band 1.5k 1k bass 20 reverb"
+sudo nice --20 play "| BUF_0_=1 BUF_1_=1 rec --buffer 256 -d pitch -500 echos 0.8 0.88 10 0.6 band 1.5k 1k bass 20 reverb"
