6334c303c8340e5ef8ae5c4eff6279ee3dc1a6da
Sebastian Butterweck <sebastian@butt3rweck.com>
2020-04-12 16:01:40 +0100

    reduced buffer size for realtime proc

--- vader.sh
+++ vader.sh
@@ -1,2 +1,2 @@
 sleep 60s
-nice --22 play "|rec --buffer 2048 -d pitch -500 echos 0.8 0.88 10 0.6 band 1.5k 1k bass 20 reverb"
+nice --22 play "|rec --buffer 512 -d pitch -500 echos 0.8 0.88 10 0.6 band 1.5k 1k bass 20 reverb"
