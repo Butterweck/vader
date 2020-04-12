c83d83569ab1f05db1b88b18c7e8970ff6b3f426
Sebastian Butterweck <sebastian@butt3rweck.com>
2020-04-12 17:27:35 +0100

    uff 2 ...

--- vader.sh
+++ vader.sh
@@ -1,2 +1,2 @@
 sleep 60s
-sudo nice --22 play "|rec --buffer 512 -d pitch -500 echos 0.8 0.88 10 0.6 band 1.5k 1k bass 20 reverb"
+sudo nice --20 play "|rec --buffer 512 -d pitch -500 echos 0.8 0.88 10 0.6 band 1.5k 1k bass 20 reverb"
