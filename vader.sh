e2e9fa47a7410290ef32b70fd4c037946d8b96a9
Sebastian Butterweck <Butterweck@users.noreply.github.com>
2021-05-03 19:23:03 +0200

    Update vader.sh

--- vader.sh
+++ vader.sh
@@ -1,5 +1,5 @@
 sleep 60s
 play /home/pi/git/vader/sf_laser_18.mp3
 while [ true ]; do
-  sudo nice --10 play --buffer 1024 "| BUF_0_=1024 BUF_1_=1024 rec --buffer 1024 -d overdrive 10 echo 0.8 0.8 5 0.7 echo 0.8 0.7 6 0.7 echo 0.8 0.7 10 0.7 echo 0.8 0.7 12 0.7 echo 0.8 0.88 12 0.7 echo 0.8 0.88 30 0.7 echo 0.6 0.6 60 0.7";
+  sudo nice --10 play --buffer 1024 "| BUF_0_=1024 BUF_1_=1024 rec --buffer 1024 -d overdrive 30 pitch -100 bass 20 reverb echo 0.8 0.88 6 0.4 ";
 done
