057b86afc06eb21aa05e0be67269352e2600f3f7
Sebastian Butterweck <Butterweck@users.noreply.github.com>
2021-05-03 17:33:54 +0200

    Update vader.sh

--- vader.sh
+++ vader.sh
@@ -2,5 +2,5 @@ sleep 60s
 #play /home/pi/git/vader/imperial.wav
 play /home/pi/git/vader/breathe.wav
 while [ true ]; do
-  sudo nice --10 play --buffer 1024 "| BUF_0_=1024 BUF_1_=1024 rec --buffer 1024 -d pitch -400 echos 0.8 0.88 10 0.6 band 1.5k 1k bass 20 reverb";
+  sudo nice --10 play --buffer 1024 "| BUF_0_=1024 BUF_1_=1024 rec --buffer 1024 -d overdrive 10 echo 0.8 0.8 5 0.7 echo 0.8 0.7 6 0.7 echo 0.8 0.7 10 0.7 echo 0.8 0.7 12 0.7 echo 0.8 0.88 12 0.7 echo 0.8 0.88 30 0.7 echo 0.6 0.6 60 0.7";
 done
